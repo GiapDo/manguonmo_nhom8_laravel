c2d48f046e5f85b484805ddacae838e34463a405
PhucNV47 <nguyenvanphuc262003@gmail.com>
2025-04-13 23:23:21 +0700

    index,slides

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,11 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index(){
-        
+        $slides = Slide::where('status', 1)->get()->take(3);
+        $categories = Category::orderBy('name')->get();
+        $sproducts = Product::whereNotNull('sale_price')->where('sale_price', '<>', '')->inRandomOrder()->get()->take(8);
+        $fproducts = Product::where('featured', 1)->get()->take(8);
+        return view('index', compact('slides', 'categories', 'sproducts', 'fproducts'));
     }
 
     public function contact(){
