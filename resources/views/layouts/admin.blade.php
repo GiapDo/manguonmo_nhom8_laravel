0445662bb7293d2e2ec742a44a725fd836a81034
GiapDo <doquangphuong.nd2003@gmail.com>
2025-04-13 23:32:03 +0700

    Sửa lần cuối

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -150,7 +150,7 @@
                         
                         <div class="footer-bottom">
                             <div class="container d-md-flex align-items-center">
-                              <span class="footer-copyright me-auto">©2024 Nhóm 8</span>
+                              <span class="footer-copyright me-auto">©2025 Nhóm 8</span>
                               <div class="footer-settings d-md-flex align-items-center">
                                 Đỗ Phương&Nguyễn Phúc
                               </div>
